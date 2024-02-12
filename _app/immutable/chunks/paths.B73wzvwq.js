39d29020c222d37f82ebc9467b490fd283cddebd
GearKite <em@em.id.lv>
2024-02-12 21:41:09 +0200

    Updates

---
+++ _app/immutable/chunks/paths.B73wzvwq.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1cp6tfb)==null?void 0:s.base)??"/guess-that-rs";var t;const e=((t=globalThis.__sveltekit_1cp6tfb)==null?void 0:t.assets)??a;export{e as a,a as b};
---
+++ _app/immutable/entry/start.qsI_1dCd.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.tFKaQAn5.js";export{t as start};
