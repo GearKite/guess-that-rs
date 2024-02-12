f0a5e060e85e88739c482e15ca81d4f3840eba9a
GearKite <onyx-commute-quit@duck.com>
2024-02-12 21:31:15 +0200

    Build static site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,26 @@
 <script>
 	import '../app.css';
 	import ThemeSwitch from '$lib/ThemeSwitch/ThemeSwitch.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <nav class="bg-gray-800 dark:bg-gray-900">
 	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
 		<div class="relative flex h-16 items-center justify-between">
 			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-				<a href="/" class="text-slate-100 text-2xl font-bold">Guess That RS</a>
+				<a href={base} class="text-slate-100 text-2xl font-bold">Guess That RS</a>
 				<div class="hidden sm:ml-6 sm:block">
 					<div class="flex space-x-4">
 						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
 						<a
-							href="/"
+							href={base}
 							class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
 							aria-current="page"
 						>
 							Home
 						</a>
 						<a
-							href="/play"
+							href="{base}/play"
 							class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
 						>
 							Play
